928941147ec89ecd58866c4b9269553a50312fb2
jalal <galalkakhi@gmail.com>
2025-05-24 14:24:39 +0200

    adding rules of physcis inside it

--- physcis.js
+++ physcis.js
@@ -1,3 +1,49 @@
-console.log("hello man")
-console.log("we will see about that okay ")
-console.log("now we will talk about git hub")
+class shout {
+    shout(V0, tita) {
+        this.V0 = V0;
+        this.tita = tita
+
+    }
+    name(params) {
+
+    }
+    DegreeToRadian(tita) {
+        return tita * (Math.PI / 180)
+
+    }
+    Vx(tita, V0) {
+        titaInRadain = this.DegreeToRadian(tita)
+        return V0 * Math.cos(titaInRadain)
+    }
+    Vy(tita, V0, t) {
+        titaInRadain = this.DegreeToRadian(tita)
+        return -9.98 * t + V0 * Math.sin(titaInRadain)
+
+    }
+    X(Vx, t) {
+        return Vx * t
+    }
+
+    Y(Vy, t) {
+        return (1 / 2) * 9.98 * t * t + Vy * t
+    }
+
+    t(x, V0, tita) {
+        return x / (Vx(V0, tita))
+    }
+
+    colculateTheShout(tita, V0, t) {
+        vx = Vx(tita, V0)
+        vy = Vy(tita, V0)
+        if (tita < 180 || tita > 0 && tita != 90) {
+            return ShoutVariables = [vx, vy, X(vx, t), Y(vy, t)]
+        }
+        if (tita == 90) {
+            return ShoutVariables = [vx, Vy(tita, V0), X = 0, Y(vy, t)]
+        }
+        if (tita == 180 || tita == 0) {
+            return ShoutVariables = [vx, vy, X(vx, t), Y(vy, t)]
+        }
+    }
+
+}
